2f5f4853998e77d41c55b69df18fb0a6205f9015
Philippe Hässig <phil@nine.ch>
2017-10-23 13:54:46 +0200

    Allow to set a redis password

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,9 @@
 require 'sidekiq/api'
 
-redis_config = { url: 'redis://redis:6379' }
+redis_config = {
+  url: 'redis://redis:6379',
+  password: ENV.fetch('REDIS_PASSWORD', nil),
+}
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
